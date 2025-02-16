9fb1b5dfa68a27f57727800cd14654b6ce4a2edd
Dmitry Golub <gosest4@gmail.com>
2025-02-16 14:12:31 +0300

    GOL-6 Add JWT Authorization

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
+from hashlib import algorithms_available
 from typing import List
+from random import choice
+import jwt
+import datetime
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field
@@ -6,11 +10,29 @@ from pydantic import BaseModel, Field
 
 app = FastAPI()
 
+SECRET_KEY = 'secret key'
+
+# типо BD
 data = {}
+# хэш таблица, в котором хранятся валидные токены. {jwt: emoji}
+jwt_to_emoji = {}
+# доступные эмодзи
+emojis = {'🤢', '😍', '👽', '🥸', '🥳', '🐵'}
+
 
 class MessageScheme(BaseModel):
-    text: str = Field(max_length=100)
-    author: str = Field(max_length=50)
+    jwt: str
+    text: str = Field(max_length=255)
+
+
+def create_jwt(data: dict) -> str:
+    """Генерируем JWT"""
+    return jwt.encode(data, SECRET_KEY, algorithm="HS256")
+
+
+def validate_jwt(token: str):
+    """Просто проверка, что jwt есть в jwt_to_emoji и пользователь может отправлять сообщения, используя его"""
+    return token in jwt_to_emoji
 
 
 @app.get(
@@ -19,6 +41,7 @@ class MessageScheme(BaseModel):
     tags=["Chat"]
 )
 def get_messages(chat_slug: str):
+    """Получаем все сообщения из чата с определенным slug"""
     if chat_slug in data:
         return data[chat_slug]
     else:
@@ -35,12 +58,50 @@ def post_message(chat_slug: str, message: MessageScheme):
     if chat_slug not in data:
         data[chat_slug] = []
 
+    # Валидация jwt
+    if not validate_jwt(message.jwt):
+        return HTTPException(status_code=404, detail={'msg': 'Incorrect JWT'})
+
     # Добавляем сообщение в чат
     data[chat_slug].append({
-        'id': len(data[chat_slug]) + 1,
+        'jwt': message.jwt,
         'text': message.text,
-        'author': message.author,
+        'emoji': jwt_to_emoji[message.jwt],
+        'timestamp': datetime.datetime.now()
     })
 
     return {'ok': True, 'msg': 'Success post message'}
+
+
+@app.get(
+    "/login/",
+    summary="Login",
+    tags=["Chat"]
+)
+def login():
+    """авторизация пользователя. Выдается личный JWT"""
+    # получаем рандомно emoji (если еще остались) из emojis. Иначе возвращаем нет доступа
+    if emojis:
+        emoji = choice(list(emojis))
+        emojis.remove(emoji)
+    else:
+        return HTTPException(status_code=404, detail={'msg': 'The limit of participants in the chat has been exceeded'})
+
+    token = create_jwt({'emoji': emoji})
+    jwt_to_emoji[token] = emoji
+
+    return {'token': token}
+
+@app.get(
+    '/check_token/{token:str}',
+    summary='Check that the token is valid and can be used',
+    tags=['Chat'],
+)
+def check_token(token: str):
+    """Проверка валидности токена"""
+    if validate_jwt(token):
+        return {'ok': True, 'msg': 'Correct JWT'}
+    else:
+        return HTTPException(status_code=404, detail={'msg': 'Incorrect JWT'})
+
 
